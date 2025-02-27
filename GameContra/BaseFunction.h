bfb7528ad387cd4d992b649bd632705c371540ef
Andrew-solfware <lnh21012005@gmail.com>
2025-02-27 10:28:33 +0700

    change

--- GameContra/BaseFunction.h
+++ GameContra/BaseFunction.h
@@ -8,18 +8,19 @@
 #include <SDL_mixer.h>
 #include <SDL_ttf.h>
 
-static SDL_Window* g_window = NULL;
-static SDL_Renderer* g_screen = NULL;
-static SDL_Event g_event;
+static SDL_Window* g_window = NULL; //build form window
+static SDL_Renderer* g_screen = NULL; // build form renderer
+static SDL_Event g_event; // event
 
 // SCREEN
-const int SCREEN_WIDTH = 1280;
-const int SCREEN_HEIGHT = 640;
+const int SCREEN_WIDTH = 1280; // Width of screen = 1280
+const int SCREEN_HEIGHT = 640; // Height of screen = 640
+// Dinh dang man hinh
 const int SCREEN_BPP = 32; // Bit per pixel
 
 
 // COLOR
-const int COLOR_KEY_R = 167;
+const int COLOR_KEY_R = 167; 
 const int COLOR_KEY_G = 175;
 const int COLOR_KEY_B = 180;
 
